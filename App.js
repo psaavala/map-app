753034340c5290343c1ac6e94eb733b455854ec3
psaavala <c3sahe00@students.oamk.fi>
2025-02-17 11:30:28 +0200

    Added icons for bottomnav, and skeleton of mapview with working search

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import react from 'react';
+import { Alert, Pressable, StyleSheet, Text, View, } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import react, { useState, useEffect } from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as Location from 'expo-location';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MapView, { Marker } from 'react-native-maps';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
+
+
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,10 +22,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Stack.Screen name="Locations" component={LocationsScreen} />
-        <Stack.Screen name="Add location" component={AddLocationScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Capitals" component={CapitalScreen} />
+        <Stack.Screen name="Locations"
+          component={LocationsScreen}
+          options={{ tabBarIcon: () => <Entypo name="location" size={24} color="black" /> }} />
+        <Stack.Screen name="Add location"
+          component={AddLocationScreen}
+          options={{ tabBarIcon: () => <AntDesign name="plus" size={26} /> }} />
+        <Stack.Screen name="Map"
+          component={MapScreen}
+          options={{ tabBarIcon: () => <Entypo name="map" size={24} color="black" /> }} />
+
+        <Stack.Screen name="Capitals"
+          component={CapitalScreen}
+          options={{ tabBarIcon: () => <FontAwesome5 name="city" size={24} color="black" /> }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -23,36 +42,84 @@ export default function App() {
 
 function LocationsScreen() {
 
-  const navigation = useNavigation();
+
 
   return (
-    <Pressable onPress={() => navigation.navigate('Locations')}>
+
     <Text variant='displayMedium'>Locations</Text>
-    </Pressable>
+
   );
 }
 
 function AddLocationScreen() {
 
-  const navigation = useNavigation();
+
 
   return (
-    <Pressable onPress={() => navigation.navigate('Add location')}>
+
     <Text variant='displayMedium'>Add Locations</Text>
-    </Pressable>
+
   );
 }
+
 
 function MapScreen() {
 
-  const navigation = useNavigation();
+
+
+  const [loc, setLoc] = useState({lat:65.0800, lon:25.4800});
+  const [place, setPlace] = useState('');
+
+  useEffect(() => {
+    getLocation();
+    async function getLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Lowest });
+      setLoc({ lat: location.coords.latitude, lon: location.coords.longitude });
+    }
+  }, []);
+
+  async function search(){
+    let coords = await Location.geocodeAsync(place);
+    if(coords[0]){
+      setLoc({lat: coords[0].latitude, lon: coords[0].longitude});
+    }else{
+      Alert.alert('Location not found!!');
+    }
+  }
 
   return (
-    <Pressable onPress={() => navigation.navigate('Map')}>
-    <Text variant='displayMedium'>Map screen will be shown here</Text>
-    </Pressable>
+    <SafeAreaView style={styles.container}>
+      <TextInput value={place} onChangeText={setPlace}/>
+      <Button onPress={search}>Search</Button>
+      <MapView
+       style={styles.map}
+        region={{
+           latitude: loc.lat,
+            longitude: loc.lon,
+             latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421 
+        }}
+      >
+        { place  !== '' &&
+          <Marker
+            title={place}
+            coordinate={{ latitude: loc.lat, longitude: loc.lon }}
+          />
+          }
+      </MapView>
+      </SafeAreaView>
   );
 }
+
+
+
 
 function CapitalScreen() {
 
@@ -60,20 +127,19 @@ function CapitalScreen() {
 
   return (
     <Pressable onPress={() => navigation.navigate('Capitals')}>
-    <Text variant='displayMedium'>This page shows capitals</Text>
+      <Text variant='displayMedium'>This page shows capitals</Text>
     </Pressable>
   );
 }
-
-
-
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  map: {
+    flex: 1,
+  }
 });
