13095ca66faa9dfd90880eb0eee47dc7a022cc70
psaavala <c3sahe00@students.oamk.fi>
2025-02-21 13:03:21 +0200

    added star rating, modified add location display, built locations display

--- App.js
+++ App.js
@@ -11,7 +11,9 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Entypo from '@expo/vector-icons/Entypo';
 import { addTodo, useFireTodos } from './firebase/FirestoreController'
-import {Button, MD3LightTheme, PaperProvider, TextInput } from 'react-native-paper';
+import { Button, MD3LightTheme, PaperProvider, TextInput } from 'react-native-paper';
+import StarRating from 'react-native-star-rating-widget';
+import { FlatList } from 'react-native';
 
 
 
@@ -43,43 +45,96 @@ export default function App() {
 
 function LocationsScreen() {
 
+  const todos = useFireTodos();
+  console.log("Todos from firestore:", todos);
+
 
 
   return (
 
-    <Text variant='displayMedium'>Locations</Text>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <FlatList
+            data={todos}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <TodoItem todoItem={item} />}
+            contentContainerStyle={styles.list}
+          />
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
+  );
+}
 
+function TodoItem({ todoItem }) {
+  return (
+    <View style={styles.card}>
+      
+      <View style={styles.header}>
+        <Text variant="titleLarge" style={styles.title}>{todoItem.todoText}</Text>
+        <Entypo name="location-pin" size={24} color="red" />
+      </View>
+
+      
+      <Text variant="bodyMedium" style={styles.review}>{todoItem.review}</Text>
+
+      
+      <StarRating rating={todoItem.stars} onChange={() => {}} starSize={24} color="gold" />
+    </View>
   );
 }
 
 function AddLocationScreen() {
 
-    const [todo, setTodo] = useState('');
-    const todos = useFireTodos();
+  const [todo, setTodo] = useState('');
+  const [stars, setStars] = useState(0);
+  const todos = useFireTodos();
+  const [review, setReview] = useState('');
+
+    function handleAddTodo(){
+
+      addTodo(todo, stars, review)
+      setTodo('');
+      setStars(0);
+      setReview('');
+
+      Alert.alert("Success", "Location added successfully!");
+  }
+    
 
 
   return (
 
-    
-
     <PaperProvider>
       <SafeAreaView style={styles.container}>
-        <Text variant='headlineMedium'>
-          Locations list ({todos ? todos.length : 0})
-        </Text>
+        
         <TextInput
-        label={'New location'}
-        value={todo}
-        onChangeText={setTodo}
-        right={
-          <TextInput.Icon
-          icon={'plus-circle'}
-          onPress={()=> addTodo(todo)}
-          size={32}
-          color={MD3LightTheme.colors.primary}
-          />
-        }
+          label={'Add new location'}
+          value={todo}
+          onChangeText={setTodo}
         />
+        <TextInput
+          label={'Add review text'}
+          value={review}
+          onChangeText={setReview}
+          />
+        <StarRating
+          rating={stars}
+          onChange={setStars}
+          starSize={24}
+          color="gold"
+        />
+        <Button
+          mode="contained"
+          icon={'plus-circle'}
+          onPress={handleAddTodo}
+          >
+            Add location
+          </Button>
+          
+
+
       </SafeAreaView>
     </PaperProvider>
 
@@ -90,26 +145,11 @@ function AddLocationScreen() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function MapScreen() {
 
 
 
-  const [loc, setLoc] = useState({lat:65.0800, lon:25.4800});
+  const [loc, setLoc] = useState({ lat: 65.0800, lon: 25.4800 });
   const [place, setPlace] = useState('');
 
   useEffect(() => {
@@ -127,36 +167,36 @@ function MapScreen() {
     }
   }, []);
 
-  async function search(){
+  async function search() {
     let coords = await Location.geocodeAsync(place);
-    if(coords[0]){
-      setLoc({lat: coords[0].latitude, lon: coords[0].longitude});
-    }else{
+    if (coords[0]) {
+      setLoc({ lat: coords[0].latitude, lon: coords[0].longitude });
+    } else {
       Alert.alert('Location not found!!');
     }
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput value={place} onChangeText={setPlace}/>
+      <TextInput value={place} onChangeText={setPlace} />
       <Button onPress={search}>Search</Button>
       <MapView
-       style={styles.map}
+        style={styles.map}
         region={{
-           latitude: loc.lat,
-            longitude: loc.lon,
-             latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421 
+          latitude: loc.lat,
+          longitude: loc.lon,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
         }}
       >
-        { place  !== '' &&
+        {place !== '' &&
           <Marker
             title={place}
             coordinate={{ latitude: loc.lat, longitude: loc.lon }}
           />
-          }
+        }
       </MapView>
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
@@ -179,9 +219,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-
+    padding: 10,
   },
   map: {
     flex: 1,
-  }
+  },
+  list: {
+    paddingBottom:20,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginVertical: 8,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  title:{
+    fontWeight: 'bold',
+  },
+  review: {
+    fontStyle: 'italic',
+    marginBottom: 10,
+  },
 });
